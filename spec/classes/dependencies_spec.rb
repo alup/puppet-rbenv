9ac9c6df8df1289b7bb20bcc25bf404db67cf8b0
Andreas Loupasakis <andreas@aloop.org>
2012-04-11 22:33:26 +0300

    Add spec for dependencies class

---
+++ spec/classes/dependencies_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'rbenv::dependencies' do
+  let(:title) { 'rbenv::dependencies' }
+
+  context 'Ubuntu' do
+    let(:facts) { { :operatingsystem => 'Ubuntu' } }
+    it { should include_class('rbenv::dependencies::ubuntu') }
+  end
+
+  context 'Debian' do
+    let(:facts) { { :operatingsystem => 'Debian' } }
+    it { should include_class('rbenv::dependencies::ubuntu') }
+  end
+
+end
