6474c745fe4ecdb65c459f8173caed98fdd6e627
Marcello Barnaba <vjt@openssl.it>
2012-07-30 15:14:07 +0200

    Add SuSE dependencies

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,5 +2,6 @@ class rbenv::dependencies {
   case $operatingsystem {
     Ubuntu,Debian: { require rbenv::dependencies::ubuntu }
     RedHat,CentOS: { require rbenv::dependencies::centos }
+    SuSE,OpenSuSE: { require rbenv::dependencies::suse }
   }
 }
