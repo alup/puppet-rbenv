0a14db53823d25fdbb812b5aea100ddec5e38b0b
Romuald du Song <romuald.dusong@deveryware.com>
2013-05-14 18:58:26 +0200

    downgrade to readline5 for lenny

--- manifests/dependencies/ubuntu.pp
+++ manifests/dependencies/ubuntu.pp
@@ -3,8 +3,16 @@ class rbenv::dependencies::ubuntu {
   if ! defined(Package['libc6-dev'])        { package { 'libc6-dev':        ensure => installed } }
   if ! defined(Package['bison'])            { package { 'bison':            ensure => installed } }
   if ! defined(Package['openssl'])          { package { 'openssl':          ensure => installed } }
-  if ! defined(Package['libreadline6'])     { package { 'libreadline6':     ensure => installed } }
-  if ! defined(Package['libreadline6-dev']) { package { 'libreadline6-dev': ensure => installed } }
+  case $::lsbdistcodename {
+    lenny: {
+      if ! defined(Package['libreadline5'])     { package { 'libreadline5':     ensure => installed } }
+      if ! defined(Package['libreadline5-dev']) { package { 'libreadline5-dev': ensure => installed } }
+    }
+    default : {
+      if ! defined(Package['libreadline6'])     { package { 'libreadline6':     ensure => installed } }
+      if ! defined(Package['libreadline6-dev']) { package { 'libreadline6-dev': ensure => installed } }
+    }
+  }
   if ! defined(Package['zlib1g'])           { package { 'zlib1g':           ensure => installed } }
   if ! defined(Package['zlib1g-dev'])       { package { 'zlib1g-dev':       ensure => installed } }
   if ! defined(Package['libssl-dev'])       { package { 'libssl-dev':       ensure => installed } }
