41437cfc53b34e75f4baab1c4b7547fa986b3e73
Larry Glenn <yrral.glenn@gmail.com>
2013-08-10 09:18:44 -0400

    Refactor rbenv__gem_spec

--- spec/defines/rbenv__gem_spec.rb
+++ spec/defines/rbenv__gem_spec.rb
@@ -4,16 +4,18 @@ describe 'rbenv::gem', :type => :define do
   # User must be unique to this spec so that our fixture doesn't break other specs.
   # User and version muct match what's in the Exec[rbenv::compile...] 
   # in spec/fixtures/manifests/site.pp so that the 'is this ruby installed' test
-  # in the defined type will pass.
-  let(:user)         { 'gem_tester' }
-  let(:ruby_version) { '1.9.3-p125' }
-  let(:title)        { 'somegem' }
-  let(:gem_name)     { title }
-  let(:rbenv)        { "/home/#{user}/.rbenv/versions/#{ruby_version}" }
-  let(:_ensure)      { 'present' }
-  let(:params)       { {:user => user, :ruby => ruby_version } }
+  # in rbenvgem.pp will pass.
+  let(:user)           { 'gem_tester' }
+  let(:ruby_version)   { '1.9.3-p125' }
+  let(:title)          { 'somegem' }
+  let(:gem_name)       { params[:gem] || title }
+  let(:rbenv)          { "/home/#{user}/.rbenv/versions/#{ruby_version}" }
+  let(:_ensure)        { params[:ensure] || 'present' }
+  let(:default_params) { {:user => user, :ruby => ruby_version } }
+  let(:params)         { default_params.merge(@params) }
 
   before do
+    @params = {}
   end
 
   describe 'when the ruby version has not been installed' do
@@ -32,45 +34,47 @@ describe 'rbenv::gem', :type => :define do
         'user' => params[:user],
         'gemname' => gem_name,
         'ruby' => params[:ruby],
-        'rbenv' => rbenv
-      ) 
+        'rbenv' => rbenv,
+    ) 
     }
-  end
+        end
 
   it_behaves_like "rbenvgem"
 
   describe "when there is an ensure param" do
-    let(:params) { { :ensure => 'absent', :user => user, :ruby => ruby_version } }
-    it_behaves_like "rbenvgem" do
-      let(:_ensure) { params[:ensure] }
+    before do
+      @params[:ensure] = 'absent'
     end
-  end
+    it_behaves_like "rbenvgem"
+    end
 
   describe "when there is a gem param" do
-    let(:params) { { :gem => 'some-other-gem', :user => user, :ruby => ruby_version } }
-    it_behaves_like "rbenvgem" do
-      let(:gem_name) { params[:gem] }
+    before do
+      @params[:gem] = 'some-other-gem'
     end
+    it_behaves_like "rbenvgem"
   end
 
   describe "when there is a home param" do
-    let(:params) { { :home => '/usr/local/foo', :user => user, :ruby => ruby_version } }
-    it_behaves_like "rbenvgem" do
-      let(:rbenv) { "#{params[:home]}/.rbenv/versions/#{ruby_version}" }
+    before do
+      @params[:home] = '/usr/local/foo'
     end
+    let(:rbenv) { "#{params[:home]}/.rbenv/versions/#{ruby_version}" }
+    it_behaves_like "rbenvgem"
   end
 
   describe "when there is a root param" do
-    let(:params) { { :root => '/usr/local/bar/.rbenv', :user => user, :ruby => ruby_version } }
-    it_behaves_like "rbenvgem" do
-      let(:rbenv) { "#{params[:root]}/versions/#{ruby_version}" }
+    let(:rbenv) { "#{params[:root]}/versions/#{ruby_version}" }
+    before do
+      @params[:root] = '/usr/local/bar/.rbenv'
     end
-  end
+    it_behaves_like "rbenvgem"
 
-  describe "when there is both a home and a root param the root takes precedence" do
-    let(:params) { { :home => '/usr/local/foo', :root => '/usr/local/bar/.rbenv', :user => user, :ruby => ruby_version } }
-    it_behaves_like "rbenvgem" do
-      let(:rbenv) { "#{params[:root]}/versions/#{ruby_version}" }
+    describe "when there is both a home and a root param the root takes precedence" do
+      before do
+        @params[:home] = '/usr/local/foo'
+      end
+      it_behaves_like "rbenvgem"
     end
   end
 
